23fa7f24ef7a9b543f1f72b6b3197df190d005ef
Marissa Bell <marissabell@westernwyoming.edu>
2024-09-09 21:58:21 -0700

    Add files via upload

---
+++ Module 3/js02/project01/m03project02-01.js
@@ -0,0 +1,17 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-01
+
+      Celsius <-> Farenheit Coverter
+      Author: Marissa Bell
+      Date:   9/8/24
+
+      Filename: project02-01.js
+ */
+
+function FarenheitToCelcius(degree){"cValue"=("fValue"-32)*5/9}
+function CelciusToFarenheit(degree){"fValue"=("cValue"*1.8)+32}
+cValueElement.onchange = function(var cDegree= cValue)
+fValueElement.onchange = function( var fDegree= fValue )
+var fValue=CelciusToFarenheit(cDegree)
+var cValue=FarenheitToCelcius(fDegree)
